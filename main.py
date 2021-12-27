4a7effcdab32bb7a82963f5d6bc8b0f92be2e7b8
Lolo <artem_ivashevich@mail.ru>
2021-12-27 20:21:10 +0300

    version 2 add chat menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from telegram import Update
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackContext
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackContext, CallbackQueryHandler
 from openpyxl import load_workbook
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 wb = load_workbook('database.xlsx')
 vendor = wb['users']
@@ -9,18 +10,133 @@ token = '1684008354:AAGtETeI2tgXd-AM_6AQvJmh43aTSYjkXxE'
 
 def main():
 
-    updater = Updater(token=token)
+    updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
 
-    handler = MessageHandler(Filters.all, do_command)
+    handler = MessageHandler(Filters.command, do_command)
+
+    # start_handler = CommandHandler('start', do_start)
+    # help_handler = CommandHandler('help', do_help)
+    keyboard_handler = MessageHandler(Filters.text, keyboard_value)
 
     dispatcher.add_handler(handler)
+    dispatcher.add_handler(keyboard_handler)
 
     updater.start_polling()
     updater.idle()
 
+def do_start(update, context):
+
+    keyboard = [
+        ["Теория"],
+        ["Практика"]
+    ]
+
+    update.message.reply_text(text='Добро пожаловать, выберите, что бы вы хотели сделать.',
+                              reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True))
+
+def keyboard_value(update, context):
+    text = update.message.text
+
+    if text == "Теория":
+        context.user_data['sort'] = 'Теория'
+        keyboard = [
+            ["Умножения двоичных чисел"],
+            ["Возведение в квадрат"],
+            ["Квадратные уравнения"]
+        ]
+        update.message.reply_text(text='Выберите тему',reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True))
+
+    elif text == "Практика":
+        context.user_data['sort'] = 'Практика'
+        keyboard = [
+            ["Умножения двоичных чисел"],
+            ["Возведение в квадрат"],
+            ["Квадратные уравнения"]
+        ]
+        update.message.reply_text(text='Выберите тему',reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True))
+
+    if 'sort' in context.user_data:
+
+        if update.message.text != 'Теория' and context.user_data['command'] == 'Теория':
+
+            if text == "Умножения двоичных чисел":
+
+                context.user_data['sort'] = 'Теория'
+                keyboard = [
+                    ["Далее"]
+                ]
+                update.message.reply_text(text='Выберите тему',
+                                          reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True,
+                                                                           resize_keyboard=True))
+
+            elif text == "Возведение в квадрат":
+
+                context.user_data['sort'] = 'Практика'
+                keyboard = [
+                    ["Умножения двоичных чисел"],
+                    ["Возведение в квадрат"],
+                    ["Квадратные уравнения"]
+                ]
+                update.message.reply_text(text='Выберите тему',
+                                          reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True,
+                                                                           resize_keyboard=True))
+
+            elif text == "Квадратные уравнения":
+
+                context.user_data['sort'] = 'Практика'
+                keyboard = [
+                    ["Умножения двоичных чисел"],
+                    ["Возведение в квадрат"],
+                    ["Квадратные уравнения"]
+                ]
+                update.message.reply_text(text='Выберите тему',
+                                          reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True,
+                                                                           resize_keyboard=True))
+
+        elif update.message.text != 'Практика' and context.user_data['command'] == 'Практика':
+
+            if text == "Умножения двоичных чисел":
+
+                context.user_data['sort'] = 'Теория'
+                keyboard = [
+                    ["Умножения двоичных чисел"],
+                    ["Возведение в квадрат"],
+                    ["Квадратные уравнения"]
+                ]
+                update.message.reply_text(text='Выберите тему',
+                                          reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True,
+                                                                           resize_keyboard=True))
+
+            elif text == "Возведение в квадрат":
+
+                context.user_data['sort'] = 'Практика'
+                keyboard = [
+                    ["Умножения двоичных чисел"],
+                    ["Возведение в квадрат"],
+                    ["Квадратные уравнения"]
+                ]
+                update.message.reply_text(text='Выберите тему',
+                                          reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True,
+                                                                           resize_keyboard=True))
+
+            elif text == "Квадратные уравнения":
+
+                context.user_data['sort'] = 'Практика'
+                keyboard = [
+                    ["Умножения двоичных чисел"],
+                    ["Возведение в квадрат"],
+                    ["Квадратные уравнения"]
+                ]
+                update.message.reply_text(text='Выберите тему',
+                                          reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True,
+                                                                           resize_keyboard=True))
+
 
 def do_command(update, context: CallbackContext):
+    if update.message.text == '/start':
+        do_start(update, context)
+        return
 
     if update.message.text == '/addven':
         context.user_data['command'] = update.message.text
